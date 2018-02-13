5eea32e000c1bec666a4ecca41b08c9622838a73
bdwheeler <bdwheeler@gmail.com>
2018-02-12 18:34:41 -0700

    update to template

--- vcenter-test-1.tf
+++ vcenter-test-1.tf
@@ -12,31 +12,31 @@ data "vsphere_datacenter" "dc" {
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "labs-qnap-240"
+  name = "labs-qnap-240"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "labs-den-prod-cluster/Resources"
+  name = "labs-den-prod-cluster/Resources"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name = "VM Network"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_template" "template" {
-  name          = Morpheus Ubuntu 16.04.3 v1"
+  name = "Morpheus Ubuntu 16.04.3 v1"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
+  name = "terraform-test"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  datastore_id = "${data.vsphere_datastore.datastore.id}"
   num_cpus = 2
-  memory   = 1024
+  memory = 1024
   guest_id = "other3xLinux64Guest"
 
   network_interface {
