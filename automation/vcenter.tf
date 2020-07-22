6af6ad157322fd35a53f2bf9e51cd550bfa453a9
Tommy <tommymarks@users.noreply.github.com>
2020-07-22 10:32:10 -0700

    Update vcenter.tf

--- automation/vcenter.tf
+++ automation/vcenter.tf
@@ -1,46 +1,48 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "vsphere" {
-  user           = "${var.cloudUsername}"
-  password       = "${var.cloudPassword}"
-  vsphere_server = "${var.cloudUrl}"
-  version        = "~> 1.3.0"
-  # if you have a self-signed cert
+  user           = var.cloudUsername
+  password       = var.cloudPassword
+  vsphere_server = var.cloudUrl
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "${var.datacenterName}"
+  name = var.datacenterName
 }
 
 data "vsphere_datastore" "datastore" {
   name = "vsanDatastore"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "pool" {
   name = "QA-vSAN/Resources"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
   name = "VLAN0002 - Internal Server"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name = "qa-apache"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm1" {
   name = "auto-vmw-terra-git1"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id = "${data.vsphere_datastore.datastore.id}"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id = data.vsphere_datastore.datastore.id
   num_cpus = 2
   memory = 1024
   guest_id = "ubuntu64Guest"
 
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
   }
 
   disk {
@@ -50,7 +52,7 @@ resource "vsphere_virtual_machine" "vm1" {
   }
 
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = data.vsphere_virtual_machine.template.id
   }
 
   connection {
@@ -62,14 +64,14 @@ resource "vsphere_virtual_machine" "vm1" {
 
 resource "vsphere_virtual_machine" "vm2" {
   name = "auto-vmw-terra-git2"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id = "${data.vsphere_datastore.datastore.id}"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id = data.vsphere_datastore.datastore.id
   num_cpus = 1
   memory = 512
   guest_id = "ubuntu64Guest"
 
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
   }
 
   disk {
@@ -79,7 +81,7 @@ resource "vsphere_virtual_machine" "vm2" {
   }
 
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = data.vsphere_virtual_machine.template.id
   }
 
   connection {
