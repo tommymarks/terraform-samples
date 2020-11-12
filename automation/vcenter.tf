c064ca5de8390b8eeb3a14192e3f10e74a0fb6a6
Tommy <tommymarks@users.noreply.github.com>
2020-11-12 12:18:16 -0800

    Update vcenter.tf

--- automation/vcenter.tf
+++ automation/vcenter.tf
@@ -7,7 +7,7 @@ provider "vsphere" {
   password       = var.cloudPassword
   vsphere_server = var.cloudUrl
   allow_unverified_ssl = true
-  version        = “~> 1.11.0”
+  version        = "~> 1.11.0"
 }
 
 data "vsphere_datacenter" "dc" {
