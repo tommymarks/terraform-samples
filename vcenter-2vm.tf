96fa716d8a5cfa0e0b93e71f28210bd62fda1f87
Tommy <tommymarks@users.noreply.github.com>
2021-02-17 19:55:46 -0800

    Update vcenter-2vm.tf

--- vcenter-2vm.tf
+++ vcenter-2vm.tf
@@ -19,7 +19,7 @@ data "vsphere_datastore" "datastore" {
 }
 
 data "vsphere_resource_pool" "pool" {
-  name = "QA-vSAN/Tests"
+  name = "QA-vSAN/Resources"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
