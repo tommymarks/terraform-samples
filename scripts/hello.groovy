4346abaa6420a06dd3b016272f7ef3704c990696
Tommy <tommymarks@users.noreply.github.com>
2021-02-18 21:38:20 -0800

    Update hello.groovy

--- scripts/hello.groovy
+++ scripts/hello.groovy
@@ -1,10 +1,8 @@
 f = new File('/var/tmp/taskGit.txt')
 f.append("groovy script\n")
-f.close()
 
-f = new File('/tmp/taskGit.txt')
-f.append("groovy script\n")
-f.close()
+f2 = new File('/tmp/taskGit.txt')
+f2.append("groovy script\n")
 
 println "HELLO GROOVY"
 return "Output GROOVY"
