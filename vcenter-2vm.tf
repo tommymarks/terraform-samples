7ec85268aac82b1753a67d7755b21f35b556dbe6
Tommy <tommymarks@users.noreply.github.com>
2024-01-31 17:25:18 -0800

    Update vcenter-2vm.tf

--- vcenter-2vm.tf
+++ vcenter-2vm.tf
@@ -24,7 +24,7 @@ data "vsphere_resource_pool" "pool" {
 }
 
 data "vsphere_network" "network" {
-  name = "VLAN0002 - Internal Server"
+  name = "VLAN0043 - QA"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
